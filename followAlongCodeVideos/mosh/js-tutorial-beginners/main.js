0b83e83d5c7cd142bea845833c794f5ec64788e1
Eric Bible <eric@ejbdevdes.com>
2023-01-21 04:41:03 -0500

    primitives w mosh

--- followAlongCodeVideos/mosh/js-tutorial-beginners/main.js
+++ followAlongCodeVideos/mosh/js-tutorial-beginners/main.js
@@ -1,3 +1,14 @@
+//variable reassignment with let and not with const
 const interestRate = 0.3
 
 console.log(interestRate);
+
+//Primitives
+
+let name = "Eric" // string literal
+let age = 34.95 // number literal
+let isCool = true //boolean literal
+let dateMarried = undefined
+let purchasedHouse = null
+
+//
