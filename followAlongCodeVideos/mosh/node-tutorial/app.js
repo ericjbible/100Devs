bf9770f3694d664028bc98f69da9e8123d9da048
Eric Bible <eric@ejbdevdes.com>
2023-02-05 06:10:25 -0500

    node w mosh

--- followAlongCodeVideos/mosh/node-tutorial/app.js
+++ followAlongCodeVideos/mosh/node-tutorial/app.js
@@ -35,11 +35,21 @@ console.log(global.message) would be undefined as there is only local modular sc
 
 // console.log(pathObj);
 
-const os = require('os');
+// const os = require('os');
 
-let totalMemory = os.totalmem();
-let freeMemory = os.freemem();
+// let totalMemory = os.totalmem();
+// let freeMemory = os.freemem();
 
-console.log(`Total Memory: ${totalMemory} and Free Memory: ${freeMemory}`)
+// console.log(`Total Memory: ${totalMemory} and Free Memory: ${freeMemory}`)
 
-console.log(`Used Memory: ${totalMemory - freeMemory}`)
+// console.log(`Used Memory: ${totalMemory - freeMemory}`)
+
+const fs = require('fs');
+
+// const files = fs.readdirSync('./');
+// console.log(files)
+
+fs.readdir('./', function(err, files) {
+  if (err) console.log('Error', err);
+  else console.log('Result', files);
+})
