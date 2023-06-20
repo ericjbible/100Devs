b6ec34657793e2beb1d58ec7a67e2ee98009d764
Eric Bible <eric@ejbdevdes.com>
2023-06-20 02:14:55 -0400

    js with mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -639,28 +639,47 @@
 // const combined = parts.join('-');
 // console.log(combined);
 
-const numbers = [ 2, 3, 1];
-numbers.sort();
-console.log(numbers);
+// const numbers = [ 2, 3, 1];
+// numbers.sort();
+// console.log(numbers);
 
-numbers.reverse();
-console.log(numbers);
+// numbers.reverse();
+// console.log(numbers);
 
-const courses = [
-  {id:1, name:'Node.js'},
-  {id:2, name:'JavaScript'},
-]
+// const courses = [
+//   {id:1, name:'Node.js'},
+//   {id:2, name:'JavaScript'},
+// ]
 
-courses.sort((a, b) => {
-  //a<b -1
-  //a>b 1
-  // a===b 0
-  const nameA = a.name.toLowerCase();
-  const nameB = b.name.toLowerCase();
+// courses.sort((a, b) => {
+//   //a<b -1
+//   //a>b 1
+//   // a===b 0
+//   const nameA = a.name.toLowerCase();
+//   const nameB = b.name.toLowerCase();
 
-  if (nameA < nameB) return -1;
-  if (nameA > nameB) return 1;
-  return 0;
+//   if (nameA < nameB) return -1;
+//   if (nameA > nameB) return 1;
+//   return 0;
+// });
+
+// console.log(courses);
+
+const numbers = [1, -1, 2, 3];
+
+const atLeastOnePositive = numbers.some(value => value >=0);
+const allPositive = numbers.every(value => value >=0);
+
+console.log(atLeastOnePositive);
+console.log(allPositive);
+
+const filtered = numbers.filter(value => value>= 0);
+
+const items = filtered.map(n => {
+  return { value: n };
 });
 
-console.log(courses);
+
+console.log(filtered);
+console.log(items);
+
