d5c408c5d0331e442d77750f967369a972e9e22f
Eric Bible <eric@ejbdevdes.com>
2023-06-26 01:50:43 -0400

    js with mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -609,10 +609,15 @@
 const first = [1,2,3];
 const second = [4,5,6];
 
-const combined = first.concat(second);
+// const combined = first.concat(second);
+const combined = [...first, ...second];
 
 console.log(combined);
 
-const slice = combined.slice();
+const copy = [...combined];
 
-console.log(slice);
+// const slice = combined.slice();
+
+// console.log(slice);
+
+
