a78823af2cff8d7e7aab35cfcff02d87d04affff
Eric Bible <eric@ejbdevdes.com>
2023-07-05 01:23:26 -0400

    js with mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -557,6 +557,17 @@
 // numbers.splice(1, 0, 2);
 // console.log(numbers);
 
-const numbers = [1, 2, 1, 3, 4];
-console.log(numbers.lastIndexOf(1));
-console.log(numbers.includes(3));
+// const numbers = [1, 2, 1, 3, 4];
+// console.log(numbers.lastIndexOf(1));
+// console.log(numbers.includes(3));
+
+const courses = [
+  {id:1, name:'a'},
+  {id:2, name:'b'}
+];
+
+const course = courses.find(function(course) {
+  return course.name = 'a';
+});
+
+console.log(course);
