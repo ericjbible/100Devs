0ca87b716e48dd8c7d3be09c826399dd020805c4
Eric Bible <eric@ejbdevdes.com>
2023-08-10 21:32:48 -0400

    js with mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -250,14 +250,22 @@
 // Math.floor(1.3)
 // 12 points => suspended
 
-checkSpeed (80);
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
+// checkSpeed (80);
+// function checkSpeed(speed) {
+//   const speedLimit = 70;
+//   const kmPerPoint = 5;
 
-  if ((Math.floor((speed - speedLimit) / kmPerPoint)) >= 12) {
-    console.log('Suspended');
-  } else {
-    console.log(`${(Math.floor((speed - speedLimit) / kmPerPoint))} points`);
+//   if ((Math.floor((speed - speedLimit) / kmPerPoint)) >= 12) {
+//     console.log('Suspended');
+//   } else {
+//     console.log(`${(Math.floor((speed - speedLimit) / kmPerPoint))} points`);
+//   }
+// }
+
+showNumbers(10);
+
+function showNumbers(limit) {
+  for(let i = 0; i <= limit; i++) {
+    (i === 0) ? console.log(i, "EVEN") : (i % 2 === 0) ? console.log(i, "EVEN") : console.log(i, "ODD");
   }
 }
