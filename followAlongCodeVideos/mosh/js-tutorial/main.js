2abdba05e3dce0679073471ae40c2f436b1e221b
Eric Bible <eric@ejbdevdes.com>
2023-08-28 20:36:20 -0400

    js with mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -175,10 +175,18 @@
 // for (let i = 0; i < 5; i++)
 //   console.log('JavaScript RULES!');
 
+// let i = 0;
+
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
 let i = 0;
 
-while (i < 5) {
-  console.log(i);
-  i++;
-}
+do {
+  if (i % 2 !== 0)
+    console.log(i);
+  i++
+ } while (i <= 5)
 
