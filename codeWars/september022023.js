fb32848dde3f1b93ca3de540defa3ac39310f5fd
Eric Bible <eric@ejbdevdes.com>
2023-09-03 07:19:11 -0400

    refined optimization

--- codeWars/september022023.js
+++ codeWars/september022023.js
@@ -20,6 +20,23 @@ let bmi = (weight, height) => {
 
 //optimization of my solution
 
-const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
-                                    bmi <= 25 ? "Normal" :
-                                    bmi <= 30 ? "Overweight" : "Obese";
+bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                              bmi <= 25 ? "Normal" :
+                              bmi <= 30 ? "Overweight" : "Obese";
+
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// function check(a, x) {
+//   // your code here
+// }
+
+//my solution
+
+const check = (a, x) => a.includes(x)
+
+
