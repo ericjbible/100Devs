8419ae809243588300ae8fce0ba850b230f3ab14
Eric Bible <eric@ejbdevdes.com>
2023-09-02 21:05:08 -0400

    09/02/23

--- codeWars/september022023.js
+++ codeWars/september022023.js
@@ -14,4 +14,4 @@
 
 let bmi = (weight, height) => {
   return ( weight / ( height*height )) <= 18.5 ? "Underweight" : ( weight / ( height*height )) <= 25.0 ? "Normal" : ( weight / ( height*height )) <= 30.0 ? "Overweight" : "Obese"
-}``
+}
