f6e11b93085449af972885ec78d400e9eb8c1b33
Eric Bible <eric@ejbdevdes.com>
2023-10-05 14:30:46 -0400

    js w mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -783,23 +783,36 @@
 //   return array.reduce((a, b) => (a > b) ? a : b);
 // }
 
-const movies = [
-  { title: 'a', year: 2018, rating: 4.5 },
-  { title: 'b', year: 2018, rating: 4.7 },
-  { title: 'c', year: 2018, rating: 3 },
-  { title: 'd', year: 2017, rating: 4.5 },
-];
+// const movies = [
+//   { title: 'a', year: 2018, rating: 4.5 },
+//   { title: 'b', year: 2018, rating: 4.7 },
+//   { title: 'c', year: 2018, rating: 3 },
+//   { title: 'd', year: 2017, rating: 4.5 },
+// ];
 
-// All the movies in 2018 with rating > 4
-// Sort them by their rating
-// Descending order
-// Pick their title
-// returns 'b' then 'a'
+// // All the movies in 2018 with rating > 4
+// // Sort them by their rating
+// // Descending order
+// // Pick their title
+// // returns 'b' then 'a'
 
-const titles = movies
-  .filter( m => m.year === 2018 && m.rating > 4)
-  .sort((a, b) => a.rating - b.rating)
-  .reverse()
-  .map( m => m.title);
+// const titles = movies
+//   .filter( m => m.year === 2018 && m.rating > 4)
+//   .sort((a, b) => a.rating - b.rating)
+//   .reverse()
+//   .map( m => m.title);
 
-console.log(titles);
+// console.log(titles);
+
+//Function Declaration
+function walk() {
+  console.log('walk');
+}
+
+//Anonymous Function Expression
+let run = function() {
+  console.log('run');
+};
+let move = run;
+run();
+move();
