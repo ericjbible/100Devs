1c68862e288cfc4e351227500c2a15794404527d
Eric Bible <eric@ejbdevdes.com>
2023-10-06 15:33:25 -0400

    js w mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -832,10 +832,7 @@
 
 // console.log(sum(.11, 2, 3, 4, 5, 10));
 
-function interest(principal, rate, years) {
-  rate = rate || 3.5;
-  years = years || 5;
-
+function interest(principal, rate = 3.5, years = 5) {
   return principal * rate / 100 * years;
 }
 
