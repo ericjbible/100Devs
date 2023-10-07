a73baf8540c0e5f3c97fc8eafe5e25478df0507d
Eric Bible <eric@ejbdevdes.com>
2023-10-07 16:51:45 -0400

    js w mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -943,13 +943,27 @@
 //   }, 0);
 // }
 
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  draw: function() {
+// Factory Function
+
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log('draw');
+    }
+  };
+
+}
+
+const circle = createCircle(1);
+
+// Constructor Function
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
     console.log('draw');
   }
-};
+}
+
+const another = new Circle(1);
