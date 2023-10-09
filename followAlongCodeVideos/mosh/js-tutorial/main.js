293cfd34d5c735ed3d7510929523322d2b05981f
Eric Bible <eric@ejbdevdes.com>
2023-10-09 09:43:00 -0400

    js w mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -1069,3 +1069,24 @@
 // //returns all members (instance + prototype)
 // for (let key in sw1) console.log(key);
 
+function Shape() {
+
+}
+
+Shape.prototype.duplicate = function() {
+  console.log('duplicate');
+}
+
+function Circle(radius) {
+  this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.draw = function() {
+  console.log('draw');
+}
+
+const s = new Shape();
+const c = new Circle(1);
