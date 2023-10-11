321c4212208aba5c62de1427fcfea824a2adc84a
Eric Bible <eric@ejbdevdes.com>
2023-10-11 13:49:01 -0400

    js w mosh

--- followAlongCodeVideos/mosh/js-tutorial/main.js
+++ followAlongCodeVideos/mosh/js-tutorial/main.js
@@ -1181,7 +1181,25 @@ function HtmlSelectElement(items = []) {
   this.removeItem = function(item) {
     this.items.splice(this.items.indexOf(item), 1);
   }
+
+  this.render = function() {
+    return `
+    <select>${this.items.map(item => `
+      <option>${item}</option>`).join('')}
+    </select>`;
+  }
 }
 
-HtmlSelectElement.prototype = new HtmlElement;
+HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+function HtmlImageElement(src) {
+  this.src = src;
+
+  this.render = function() {
+    return `<img src="${this.src}" />`;
+  }
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
