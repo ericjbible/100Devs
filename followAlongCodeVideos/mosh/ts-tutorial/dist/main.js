38dc1d784d58838b29b3d0e85108645293454335
Eric Bible <eric@ejbdevdes.com>
2023-10-13 14:27:02 -0400

    typescript w mosh

---
+++ followAlongCodeVideos/mosh/ts-tutorial/dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 35;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=main.js.map
---
+++ followAlongCodeVideos/mosh/ts-tutorial/src/main.ts
@@ -0,0 +1,10 @@
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level;
+
+function render(document: any) {
+  console.log(document);
+}
+
+let numbers = []
