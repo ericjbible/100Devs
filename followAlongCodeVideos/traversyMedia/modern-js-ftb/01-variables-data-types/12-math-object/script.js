473be14e58d1cf5e9e0119e18fc421902408a63f
Eric Bible <eric@ejbdevdes.com>
2023-10-16 16:13:59 -0400

    js w brad traversy

---
+++ followAlongCodeVideos/traversyMedia/modern-js-ftb/01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,25 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(5.87);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(5.9);
+
+x = Math.pow(2, 3);
+
+x = Math.min(4, 5);
+
+x = Math.max(5, 9);
+
+x = Math.random();
+
+let y = Math.ceil(Math.random() * 10);
+
+console.log(y);
+
+console.log(x);
