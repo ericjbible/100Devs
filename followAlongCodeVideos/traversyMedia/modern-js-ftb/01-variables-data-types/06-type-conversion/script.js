53a1f2563fa6278db47d92266d5f3c75ab0cd13b
Eric Bible <eric@ejbdevdes.com>
2023-10-16 15:02:10 -0400

    js w brad traversy

--- followAlongCodeVideos/traversyMedia/modern-js-ftb/01-variables-data-types/06-type-conversion/script.js
+++ followAlongCodeVideos/traversyMedia/modern-js-ftb/01-variables-data-types/06-type-conversion/script.js
@@ -1,7 +1,18 @@
-let amount = '100';
+let amount = 1;
 
+// Convert string to number
 // amount = parseInt(amount);
 // amount = +amount;
 // amount = Number(amount);
+
+// Convert number to a string
+// amount = amount.toString();
+// amount = String(amount);
+
+// Convert string to decimal
+// amount = parseFloat(amount);
+
+// Convert number to boolean
+// amount = Boolean(amount);
 
 console.log(amount, typeof amount);
