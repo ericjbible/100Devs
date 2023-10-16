6ae70433c3dc2907ce57e27072616d18407999fb
Eric Bible <eric@ejbdevdes.com>
2023-10-16 15:55:30 -0400

    js w brad traversy

---
+++ followAlongCodeVideos/traversyMedia/modern-js-ftb/01-variables-data-types/09-strings/script.js
@@ -0,0 +1,48 @@
+let x;
+
+const name = 'John';
+
+const age = 30;
+
+x = 'Hello, my name is ' + name + ' ant I am ' + age + ' years old';
+
+// Template Literals
+
+x = `Hello, my name is ${name} and I am ${age} years old.`;
+
+// String Properties and Methods
+
+const s = 'Hello World';
+
+x = s.length;
+
+x = s[0];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+
+x = s.charAt(0);
+
+x = s.indexOf('e');
+
+x = s.substring(0, 4);
+
+x = s.slice(0, 5);
+
+x = s.slice(-6);
+
+x = '          hello world';
+
+x = x.trim();
+
+x = s.replace('World', 'John');
+
+x = s.includes('Hell');
+
+x = s.valueOf();
+
+x = s.split('');
+
+console.log(x);
