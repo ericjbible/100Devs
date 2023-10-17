012a5121001b64a40b69ae772f2bf0487ce9032f
Eric Bible <eric@ejbdevdes.com>
2023-10-17 13:00:51 -0400

    js w brad traversy

--- followAlongCodeVideos/traversyMedia/modern-js-ftb/02-arrays-and-objects/06-object-spread-methods/script.js
+++ followAlongCodeVideos/traversyMedia/modern-js-ftb/02-arrays-and-objects/06-object-spread-methods/script.js
@@ -6,6 +6,15 @@ todo.id = 1;
 todo.name = 'Buy Milk';
 todo.completed = false;
 
-x = todo;
+const todos = [
+  { id: 1, name: 'Eat Breakfast' },
+  { id: 2, name: 'Eat Lunch' },
+  { id: 3, name: 'Eat Dinner' },
+];
+
+x = Object.keys(todo).length;
+x = Object.values(todo);
+x = Object.entries(todo);
+x = todo.hasOwnProperty('name');
 
 console.log(x);
