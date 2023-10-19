e53ef8dafbe22d78c7befc447a037bd9f4c961aa
Eric Bible <eric@ejbdevdes.com>
2023-10-18 22:56:15 -0400

    js w brad traversy

---
+++ followAlongCodeVideos/traversyMedia/modern-js-ftb/04-logic-control-flow/04-calculator-challenge/script.js
@@ -0,0 +1,15 @@
+const calculator = (num1, num2, operator) => {
+  if (operator === '+') {
+    return num1 + num2;
+  } else if (operator === '-') {
+    return num1 - num2;
+  } else if (operator === '*') {
+    return num1 * num2;
+  } else if (operator === '/') {
+    return num1 / num2;
+  } else {
+    return 'not a valid operation';
+  }
+};
+
+console.log(calculator(5, 5, '&'));
