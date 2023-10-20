14042055aec2ec93325de4335b8e480046f3d221
Eric Bible <eric@ejbdevdes.com>
2023-10-20 15:31:14 -0400

    js w brad traversy

---
+++ followAlongCodeVideos/traversyMedia/modern-js-ftb/06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,12 @@
+// console.log(window)
+// console.log(window.document);
+
+// console.log(document.body.innerHTML);
+// console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>Hello World</h1>';
+
+const main = document.getElementById('main');
+main.innerHTML = '<h1>Hello from main</h1>';
+
+document.querySelector('#main h1').innerText = 'HOWDY';
