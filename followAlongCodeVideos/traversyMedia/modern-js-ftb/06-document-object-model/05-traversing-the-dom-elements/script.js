3f8d392e424302dfc5eff996312d71c28a83d97c
Eric Bible <eric@ejbdevdes.com>
2023-10-23 16:32:35 -0400

    js w brad traversy

--- followAlongCodeVideos/traversyMedia/modern-js-ftb/06-document-object-model/05-traversing-the-dom-elements/script.js
+++ followAlongCodeVideos/traversyMedia/modern-js-ftb/06-document-object-model/05-traversing-the-dom-elements/script.js
@@ -5,6 +5,8 @@ const parent = document.querySelector('.parent');
 
 output = parent.children;
 
-output = parent.children[1];
+output = parent.children[1].innerText;
+output = parent.children[1].className;
+output = parent.children[1].nodeName;
 
 console.log(output);
