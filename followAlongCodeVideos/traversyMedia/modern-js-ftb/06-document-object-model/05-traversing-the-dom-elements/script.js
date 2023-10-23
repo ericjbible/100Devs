c8628a20c6fa183dda85e116569493e793ac0940
Eric Bible <eric@ejbdevdes.com>
2023-10-23 18:30:13 -0400

    js w brad traversy

--- followAlongCodeVideos/traversyMedia/modern-js-ftb/06-document-object-model/05-traversing-the-dom-elements/script.js
+++ followAlongCodeVideos/traversyMedia/modern-js-ftb/06-document-object-model/05-traversing-the-dom-elements/script.js
@@ -15,4 +15,20 @@ parent.children[1].style.color = 'red';
 parent.firstElementChild.innerText = 'Child One';
 parent.lastElementChild.innerText = 'Child Three';
 
+// Get parent elements from a child
+
+const child = document.querySelector('.child');
+
+output = child.parentElement;
+
+child.parentElement.style.border = '1px solid #ccc';
+child.parentElement.style.padding = '10px';
+
+// Sibling elements
+
+const secondItem = document.querySelector('.child:nth-child(2)');
+
+output = secondItem.nextElementSibling;
+secondItem.nextElementSibling.style.color = 'green';
+secondItem.previousElementSibling.style.color = 'orange';
 console.log(output);
